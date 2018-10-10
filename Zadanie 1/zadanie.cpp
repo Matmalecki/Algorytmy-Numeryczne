f0d66680e6154c1f84ac92936147b636f587a2bf
Mateusz Małecki <mmateusz314@gmail.com>
2018-10-10 23:14:22 +0200

    Added args handling

--- Zadanie 1/zadanie.cpp
+++ Zadanie 1/zadanie.cpp
@@ -4,33 +4,60 @@
 #include <iomanip>
 #include <limits>
 #include <math.h>
+#include <stdlib.h>
 #include "Sin.h"
 #include "Cos.h"
-
+#include "string.h"
 
 double BladBezwzgledny(double myOutput, double MathhOutput)
 {
   return fabs(myOutput - MathhOutput);
 }
 
-
-
 using namespace std;
 
-int main()
+int main(int argc, char * argv[])
 {
-  int N = 100;
+
+  int N = 50;
   double x;
   int iterations = 1000000;
+  double startingValue = 0;
+  double endValue = 2 * M_PI;
+  string nameOfOutput = "AvgError.txt";
+  if (argc > 1)
+  {
+    if (!strcmp(argv[1],"--help"))
+    {
+      cout << "N - liczba sumowanych skladnikow" << endl;
+      cout << "Brak argumentow - ustawienia domyslne:" << endl;
+      cout << "Argumenty od 0 do 2PI oraz N = 50" << endl;
+      cout << "(Opcjonalnie) Pierwszy arg - liczba N" << endl;
+      cout << "(Opcjonalnie) Drugi arg - \"smallArgs\" Argumenty od 0 do 2PI/10000" << endl;
+      return 0;
+    }
+    N = atoi(argv[1]);
+
+    if (argc > 2)
+    {
+      if (!strcmp(argv[2],"smallArgs"))
+      {
+        endValue = (2 * M_PI)/10000;
+        nameOfOutput = "AvgErrorForSmallArgs.txt";
+      }
+    }
+  }
+  double plu = (endValue-startingValue)/iterations;
+
   Sin sinus;
   Cos cosin;
   ofstream oFile;
   oFile.open("wyniki.txt");
   if (oFile.is_open())
   {
-    double plu = 2*M_PI/iterations;
+
     oFile << setprecision(std::numeric_limits<double>::digits10 + 1);
-    for (x = 0; x < 2 * M_PI; x += plu )
+    for (x = startingValue; x < endValue; x += plu )
     {
       double FromMathh = sin(cos(x));
       oFile << BladBezwzgledny(sinus.SumTaylorFromBegin(cosin.SumTaylorFromBegin(x,N), N), FromMathh) << endl;
@@ -43,6 +70,9 @@ int main()
   oFile.close();
   ifstream iFile;
   iFile.open("wyniki.txt");
+  ofstream outputError;
+  outputError.open(nameOfOutput.c_str());
+  outputError << setprecision(std::numeric_limits<double>::digits10 + 1);
   double blad[4];
   for(int i = 0 ; i < 4; i++)
     blad[i] = 0.0;
@@ -57,7 +87,8 @@ int main()
       cout.precision(std::numeric_limits<double>::digits10 + 1);
       for(int i = 0 ; i < 4; i++){
         blad[i]/=iterations;
-        cout << blad[i] << endl;
+        cout  << blad[i] << endl;
+        outputError << blad[i] << endl;
       }
   }
 
